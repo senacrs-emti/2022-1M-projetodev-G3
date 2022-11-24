4f95b91173788ed66114718d16a0e2fffc1dd215
Yasser <103062506+Kleidin@users.noreply.github.com>
2022-11-24 09:25:52 -0300

    .

--- Simulador-climático/script.js
+++ Simulador-climático/script.js
@@ -1,4 +1,4 @@
-const mymap = L.map('mapid').setView([51.508, -0.11], 13);
+const mymap = L.map('mapid').setView([ -30.033056, -51.230000], 11);
 
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -6,115 +6,226 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(mymap);
 
 function m1() {
-
-    var circle = L.circle([51.508, -0.11], {
+    var circle = L.circle([ -30.033056, -51.230000], {
         color: 'red',
         fillColor: '#f03',
         fillOpacity: 0.5,
-        radius: 500
+        radius: 4000
     }).addTo(mymap);
-
-
 }
 
 function m2() {
-
-    var circle = L.circle([51.508, -0.11], {
+    var circle = L.circle([ -30.033056, -51.230000], {
         color: 'red',
         fillColor: '#f03',
         fillOpacity: 0.5,
-        radius: 500
+        radius: 4500
     }).addTo(mymap);
-
-
 }
 
 function m3() {
-
-    var circle = L.circle([51.508, -0.11], {
+    var circle = L.circle([ -30.033056, -51.230000], {
         color: 'red',
         fillColor: '#f03',
         fillOpacity: 0.5,
-        radius: 500
+        radius: 5000
     }).addTo(mymap);
-
-
 }
 
 function m4() {
-
-    var circle = L.circle([51.508, -0.11], {
+    var circle = L.circle([ -30.033056, -51.230000], {
         color: 'red',
         fillColor: '#f03',
         fillOpacity: 0.5,
-        radius: 500
+        radius: 5500
     }).addTo(mymap);
-
-
 }
 
 function m5() {
-
-    var circle = L.circle([51.508, -0.11], {
+    var circle = L.circle([ -30.033056, -51.230000], {
         color: 'red',
         fillColor: '#f03',
         fillOpacity: 0.5,
-        radius: 500
+        radius: 6000
     }).addTo(mymap);
-
-
 }
 
 function m6() {
-
-    var circle = L.circle([51.508, -0.11], {
+    var circle = L.circle([ -30.033056, -51.230000], {
         color: 'red',
         fillColor: '#f03',
         fillOpacity: 0.5,
-        radius: 100000
+        radius: 6500
     }).addTo(mymap);
-
-
 }
 
 function m7() {
-
-    var circle = L.circle([51.508, -0.11], {
+    var circle = L.circle([ -30.033056, -51.230000], {
         color: 'red',
         fillColor: '#f03',
         fillOpacity: 0.5,
-        radius: 500
+        radius: 7000
     }).addTo(mymap);
-
-
 }
 
 function m8() {
-
-    var circle = L.circle([51.508, -0.11], {
+    var circle = L.circle([ -30.033056, -51.230000], {
         color: 'red',
         fillColor: '#f03',
         fillOpacity: 0.5,
-        radius: 500
+        radius: 7500
     }).addTo(mymap);
-
-
 }
 
 function m9() {
-
-    var circle = L.circle([51.508, -0.11], {
+    var circle = L.circle([ -30.033056, -51.230000], {
         color: 'red',
         fillColor: '#f03',
         fillOpacity: 0.5,
-        radius: 500
+        radius: 8000
     }).addTo(mymap);
-
-
 }
 
+function TS1() {
+    var circle = L.circle([ -30.033056, -51.230000], {
+        color: 'red',
+        fillColor: '#f03',
+        fillOpacity: 0.5,
+        radius: 2000
+    }).addTo(mymap);
+}
+
+function TS2() {
+    var circle = L.circle([ -30.033056, -51.230000], {
+        color: 'red',
+        fillColor: '#f03',
+        fillOpacity: 0.5,
+        radius: 2500
+    }).addTo(mymap);
+}
+
+function TS3() {
+    var circle = L.circle([ -30.033056, -51.230000], {
+        color: 'red',
+        fillColor: '#f03',
+        fillOpacity: 0.5,
+        radius: 3000
+    }).addTo(mymap);
+}
+
+function TS4() {
+    var circle = L.circle([ -30.033056, -51.230000], {
+        color: 'red',
+        fillColor: '#f03',
+        fillOpacity: 0.5,
+        radius: 5500
+    }).addTo(mymap);
+}
+
+function TS5() {
+    var circle = L.circle([ -30.033056, -51.230000], {
+        color: 'red',
+        fillColor: '#f03',
+        fillOpacity: 0.5,
+        radius: 6000
+    }).addTo(mymap);
+}
+
+function TS6() {
+    var circle = L.circle([ -30.033056, -51.230000], {
+        color: 'red',
+        fillColor: '#f03',
+        fillOpacity: 0.5,
+        radius: 6500
+    }).addTo(mymap);
+}
+
+function TS7() {
+    var circle = L.circle([ -30.033056, -51.230000], {
+        color: 'red',
+        fillColor: '#f03',
+        fillOpacity: 0.5,
+        radius: 7000
+    }).addTo(mymap);
+}
+
+function TS8() {
+    var circle = L.circle([ -30.033056, -51.230000], {
+        color: 'red',
+        fillColor: '#f03',
+        fillOpacity: 0.5,
+        radius: 7500
+    }).addTo(mymap);
+}
+
+function TS9() {
+    var circle = L.circle([ -30.033056, -51.230000], {
+        color: 'red',
+        fillColor: '#f03',
+        fillOpacity: 0.5,
+        radius: 8000
+    }).addTo(mymap);
+}
+
+//Tsunami//
+
+function T1() {
+    var circle = L.circle([ -30.033056, -51.230000], {
+        color: 'red',
+        fillColor: '#f03',
+        fillOpacity: 0.5,
+        radius: 1000
+    }).addTo(mymap);
+}
+
+function T2() {
+    var circle = L.circle([ -30.033056, -51.230000], {
+        color: 'red',
+        fillColor: '#f03',
+        fillOpacity: 0.5,
+        radius: 1500
+    }).addTo(mymap);
+}
+
+function T3() {
+    var circle = L.circle([ -30.033056, -51.230000], {
+        color: 'red',
+        fillColor: '#f03',
+        fillOpacity: 0.5,
+        radius: 2000
+    }).addTo(mymap);
+}
+
+function T4() {
+    var circle = L.circle([ -30.033056, -51.230000], {
+        color: 'red',
+        fillColor: '#f03',
+        fillOpacity: 0.5,
+        radius: 5500
+    }).addTo(mymap);
+}
+
+function T5() {
+    var circle = L.circle([ -30.033056, -51.230000], {
+        color: 'red',
+        fillColor: '#f03',
+        fillOpacity: 0.5,
+        radius: 6000
+    }).addTo(mymap);
+}
+
+function T6() {
+    var circle = L.circle([ -30.033056, -51.230000], {
+        color: 'red',
+        fillColor: '#f03',
+        fillOpacity: 0.5,
+        radius: 7000
+    }).addTo(mymap);
+}
+
+//Tornado//
 
 
-var marker = L.marker([51.5, -0.09]).addTo(mymap);
+var marker = L.marker([ -30.033056, -51.230000]).addTo(mymap);
 //
 
